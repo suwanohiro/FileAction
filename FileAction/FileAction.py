b681e8ec71c8236577aea8bc024cc3bceaa7d314
suwanohiro <supermariomaker1110@gmail.com>
2024-05-10 18:42:45 +0900

    コメントを追加

--- FileAction/FileAction.py
+++ FileAction/FileAction.py
@@ -3,13 +3,31 @@ import os
 
 
 def ConvertFileLink(CurrentFilePath):
+    """
+    相対パスを絶対パスに変換する
+
+    Args:
+        CurrentFilePath (str) : 相対パス
+
+    Return:
+        str: 絶対パス
+    """
     data = os
     base = data.path.dirname(sys.argv[0])
     name = data.path.normpath(data.path.join(base, CurrentFilePath))
     return name
 
 
-def Read(CurrentFilePath):
+def Read(CurrentFilePath: str):
+    """
+    テキストファイルの読み込みをする
+
+    Args:
+        CurrentFilePath (str) : 相対パス
+
+    Return:
+        読み込んだファイルのテキストデータ
+    """
     path = ConvertFileLink(CurrentFilePath)
     Files = open(path, "r", encoding="utf-8-sig")
     result = Files.read()
@@ -18,6 +36,15 @@ def Read(CurrentFilePath):
 
 
 def ReadCSV(CurrentFilePath):
+    """
+    CSVファイルを読み込み2次元配列を返す
+
+    Args:
+        CurrentFilePath (str) : 相対パス
+
+    Return:
+        2次元配列にしたCSVファイルのデータ
+    """
     TextData = Read(CurrentFilePath)
     Result = []
     Array1 = TextData.split("\n")
@@ -30,6 +57,14 @@ def ReadCSV(CurrentFilePath):
 
 
 def Write(CurrentFilePath, WriteString, FileReadMode):
+    """
+    テキストファイルに書き込む
+
+    Args:
+        CurrentFilePath (str)   : 相対パス
+        WriteString     (str)   : 書き込むテキストデータ
+        FileReadMode    (str)   : ファイルの読み込みモード
+    """
     path = ConvertFileLink(CurrentFilePath)
     Files = open(path, FileReadMode, encoding="UTF-8")
     Files.write(WriteString)
@@ -37,12 +72,32 @@ def Write(CurrentFilePath, WriteString, FileReadMode):
 
 
 def WriteAdd(CurrentFilePath, WriteString):
+    """
+    テキストファイルに書き込む (追記)
+
+    Args:
+        CurrentFilePath (str)   : 相対パス
+        WriteString     (str)   : 書き込むテキストデータ
+    """
     Write(CurrentFilePath, WriteString, "a")
 
 
 def WriteNew(CurrentFilePath, WriteString):
+    """
+    テキストファイルに書き込む (上書き)
+
+    Args:
+        CurrentFilePath (str)   : 相対パス
+        WriteString     (str)   : 書き込むテキストデータ
+    """
     Write(CurrentFilePath, WriteString, "w")
 
 
 def Clear(CurrentFilePath):
+    """
+    テキストファイルの中身をクリアする
+
+    Args:
+        CurrentFilePath (str)   : 相対パス
+    """
     WriteNew(CurrentFilePath, "")
